229cb13fa836e521d4d7718faab3283061f9b02d
jingwu1986@hotmail.com <jingwu1986@hotmail.com>
2022-11-01 08:26:11 +0800

    init add files

---
+++ Model/GeometryInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.Types;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WktViewer.Model
+{
+    internal class GeometryInfo
+    {
+        internal OpenGisGeometryType Type { get; set; }
+
+        internal List<PointF> Points = new List<PointF>();
+        internal List<GeometryInfoItem> Items { get; set; } = new List<GeometryInfoItem>();
+
+        internal List<GeometryInfo> Collection = new List<GeometryInfo>();
+
+    }
+
+    internal class GeometryInfoItem
+    {
+        internal List<PointF> Points = new List<PointF>();
+    }
+}
---
+++ Model/GeometryViewportInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WktViewer.Model
+{
+    internal struct GeometryViewportInfo
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float MaxX { get { return this.Width / 2; } }
+        public float MaxY { get { return this.Height / 2; } }
+    }
+}
